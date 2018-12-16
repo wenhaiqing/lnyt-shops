e2c720d534e4d60334da1cc332140c2fef7102b0
wenhaiqing <243083741@qq.com>
2018-12-16 18:03:57 +0800

    前台商品列表支持按类目筛选

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -33,7 +33,7 @@ class Category extends Model
 
     public function children()
     {
-        return $this->hasMany(Category::class);
+        return $this->hasMany(Category::class,'parent_id');
     }
 
     public function products()

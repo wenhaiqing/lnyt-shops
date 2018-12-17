745e7fb7870f234e28189d578c14f4952d4ba831
wenhaiqing <243083741@qq.com>
2018-12-17 10:42:04 +0800

    后台众筹商品管理

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -73,7 +73,7 @@ class ProductsController extends Controller
     protected function grid()
     {
         return Admin::grid(Product::class, function (Grid $grid) {
-
+            $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
             $grid->id('ID')->sortable();
 
             $grid->title('商品名称');
@@ -101,7 +101,8 @@ class ProductsController extends Controller
     protected function form()
     {
         return Admin::form(Product::class, function (Form $form) {
-
+            // 在表单中添加一个名为 type，值为 Product::TYPE_NORMAL 的隐藏字段
+            $form->hidden('type')->value(Product::TYPE_NORMAL);
             // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
             $form->text('title', '商品名称')->rules('required');
 
